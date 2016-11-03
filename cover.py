5ac002b80196e40d7cbc8473fb579669ba6b39cf
Juan Lopez <jc.lopez11@uniandes.edu.co>
2016-11-03 13:51:20 -0500

    multiple points can be added to the covred list

--- cover.py
+++ cover.py
@@ -8,7 +8,7 @@ inner = 0.5
 cells = np.array([])
 cells = np.append(cells,[hexagon.Hexagon(np.array([0.,0.]),r,1,4,0,inner)])
 
-points = np.array([[2,2],[0,2],[-2,2],[-2,0],[-2,-2]])
+points = np.array([[2,2],[0,2],[-2,2],[-2,0],[-2,-2],[0,-2],[2,-2]])
 index = 1
 
 
@@ -55,13 +55,9 @@ while(doing):
                         outer_band = 1
                     cells = np.concatenate([cells,[hexagon.Hexagon(origin,r,outer_band,4,index,inner)]])
                     index += 1
-                    if(cells[-1].inpoly(k)):
-                        if(listing.ndim==2):
-                            listing = np.concatenate([listing,[k]])
-                        elif(listing.shape[0]==0):
-                            listing = k
-                        else:
-                            listing = np.concatenate([[listing],[k]])
+                    for c in points:
+                        if(cells[-1].inpoly(c)):
+                            listing = np.concatenate([listing,[c]])
             elif((clos[0]==2 and clos[1]==1) or (clos[0]==1 and clos[1]==2)):
                 origin = np.array([0,2*r*np.sin(np.pi/3.)])+cells[cell].origin
                 temp = False
@@ -78,13 +74,9 @@ while(doing):
                         outer_band = 2
                     cells = np.concatenate([cells,[hexagon.Hexagon(origin,r,outer_band,4,index,inner)]])
                     index += 1
-                    if(cells[-1].inpoly(k)):
-                        if(listing.ndim==2):
-                            listing = np.concatenate([listing,[k]])
-                        elif(listing.shape[0]==0):
-                            listing = k
-                        else:
-                            listing = np.concatenate([[listing],[k]])
+                    for c in points:
+                        if(cells[-1].inpoly(c)):
+                            listing = np.concatenate([listing,[c]])
             elif((clos[0]==3 and clos[1]==2) or (clos[0]==2 and clos[1]==3)):
                 origin = np.array([-1.5*r,r*np.sin(np.pi/3.)])+cells[cell].origin
                 temp = False
@@ -101,13 +93,9 @@ while(doing):
                         outer_band = 1
                     cells = np.concatenate([cells,[hexagon.Hexagon(origin,r,outer_band,4,index,inner)]])
                     index += 1
-                    if(cells[-1].inpoly(k)):
-                        if(listing.ndim==2):
-                            listing = np.concatenate([listing,[k]])
-                        elif(listing.shape[0]==0):
-                            listing = k
-                        else:
-                            listing = np.concatenate([[listing],[k]])
+                    for c in points:
+                        if(cells[-1].inpoly(c)):
+                            listing = np.concatenate([listing,[c]])
             elif((clos[0]==4 and clos[1]==3) or (clos[0]==3 and clos[1]==4)):
                 origin = np.array([-1.5*r,-r*np.sin(np.pi/3.)])+cells[cell].origin
                 temp = False
@@ -124,13 +112,9 @@ while(doing):
                         outer_band = 2
                     cells = np.concatenate([cells,[hexagon.Hexagon(origin,r,outer_band,4,index,inner)]])
                     index += 1
-                    if(cells[-1].inpoly(k)):
-                        if(listing.ndim==2):
-                            listing = np.concatenate([listing,[k]])
-                        elif(listing.shape[0]==0):
-                            listing = k
-                        else:
-                            listing = np.concatenate([[listing],[k]])
+                    for c in points:
+                        if(cells[-1].inpoly(c)):
+                            listing = np.concatenate([listing,[c]])
             elif((clos[0]==5 and clos[1]==4) or (clos[0]==4 and clos[1]==5)):
                 origin = np.array([0,-2*r*np.sin(np.pi/3.)])+cells[cell].origin
                 temp = False
@@ -147,13 +131,9 @@ while(doing):
                         outer_band = 1
                     cells = np.concatenate([cells,[hexagon.Hexagon(origin,r,outer_band,4,index,inner)]])
                     index += 1
-                    if(cells[-1].inpoly(k)):
-                        if(listing.ndim==2):
-                            listing = np.concatenate([listing,[k]])
-                        elif(listing.shape[0]==0):
-                            listing = k
-                        else:
-                            listing = np.concatenate([[listing],[k]])
+                    for c in points:
+                        if(cells[-1].inpoly(c)):
+                            listing = np.concatenate([listing,[c]])
             else:
                 origin = np.array([1.5*r,-r*np.sin(np.pi/3.)])+cells[cell].origin
                 temp = False
@@ -170,14 +150,12 @@ while(doing):
                         outer_band = 2
                     cells = np.concatenate([cells,[hexagon.Hexagon(origin,r,outer_band,4,index,inner)]])
                     index += 1
-                    if(cells[-1].inpoly(k)):
-                        if(listing.ndim==2):
-                            listing = np.concatenate([listing,[k]])
-                        elif(listing.shape[0]==0):
-                            listing = k
-                        else:
-                            listing = np.concatenate([[listing],[k]])
+                    for c in points:
+                        if(cells[-1].inpoly(c)):
+                            listing = np.concatenate([listing,[c]])
     print(index,cells[-1].origin,k,listing)
+
+print(cells.shape)
 
 #x = np.random.random(
         
