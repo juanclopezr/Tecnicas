9b67dcfb9858e420a051237186514f1310b23571
Juan Lopez <jc.lopez11@uniandes.edu.co>
2016-11-08 18:56:33 -0500

    calculation of SINR norm for all users without normalization

---
+++ automatized.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+inner = np.linspace(0.01,0.99,100)
+SINR = np.array([])
+
+for i in range(inner.shape[0]):
+    os.system('python cover.py 1. %f SINR%05d' % (inner[i],i))
+    data = np.loadtxt('SINR%05d' % (i))
+    SINR = np.append(SINR,np.dot(data,data)**2)
+
+plt.plot(inner,10*np.log10(SINR))
+plt.savefig('SINR.png')
--- cover.py
+++ cover.py
@@ -2,10 +2,11 @@ import hexagon
 import matplotlib.pyplot as plt
 import numpy as np
 import user
+import sys
 
-r = 1.
-inner = 0.5
-power = 20e-3
+r = float(sys.argv[1])
+inner = float(sys.argv[2])
+power = 80e-4
 
 cells = np.array([])
 cells = np.append(cells,[hexagon.Hexagon(np.array([0.,0.]),r,1,4,0,inner)])
@@ -25,10 +26,10 @@ while(doing):
         for p in listing:
             if(np.array_equal(k,p)):
                 there = True
-                print(k,p)
+                #print(k,p)
         if(not there):
             doing = True
-            print('in?')
+            #print('in?')
             distance = 1e6
             cell = np.NaN
             for j in range(cells.shape[0]):
@@ -167,9 +168,9 @@ while(doing):
                             listing = np.concatenate([listing,[c]])
                 else:
                     listing = np.concatenate([listing,[k]])
-        print(index,cells[-1].origin,k,listing)
+       # print(index,cells[-1].origin,k,listing)
 
-print(cells.shape)
+#print(cells.shape)
 
 distr = np.random.random((1000,2))*8-4
 
@@ -185,7 +186,7 @@ for i in distr:
     g = 0
     h = 0
     added = False
-    rec = 1e-7
+    rec = 6e-14
     signal = 0
     signal0 = 0
     for j in cells:
@@ -216,10 +217,10 @@ for i in distr:
             SINR = np.append(SINR,[signal/rec])
 
 print(users.shape,SINR.shape)
-np.savetxt('SINR.dat',SINR)
+np.savetxt(sys.argv[3],SINR)
 
 
-        
+"""        
 plt.fill(cells[1].corners[0],cells[1].corners[1],fill=False)
 plt.fill(cells[0].corners[0],cells[0].corners[1],fill=False)
 plt.fill(cells[2].corners[0],cells[2].corners[1],fill=False)
@@ -233,10 +234,9 @@ plt.fill(cells[9].corners[0],cells[9].corners[1],fill=False)
 plt.fill(cells[10].corners[0],cells[10].corners[1],fill=False)
 plt.fill(cells[11].corners[0],cells[11].corners[1],fill=False)
 plt.fill(cells[12].corners[0],cells[12].corners[1],fill=False)
-plt.plot(2,2,marker='o')
-plt.plot(2,0,marker='o')
-plt.plot(-2,0,marker='o')
+#plt.plot(2,2,marker='o')
+#plt.plot(2,0,marker='o')
+#plt.plot(-2,0,marker='o')
 plt.scatter(serviced[:,0],serviced[:,1])
-plt.show()
-
-
+plt.savefig('Strict-FFR')
+"""
