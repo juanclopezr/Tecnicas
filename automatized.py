f7596d7385cbf43e982ea731565805870017e8bb
Juan Lopez <jc.lopez11@uniandes.edu.co>
2016-11-08 19:16:02 -0500

    the data is normalized

--- automatized.py
+++ automatized.py
@@ -8,7 +8,7 @@ SINR = np.array([])
 for i in range(inner.shape[0]):
     os.system('python cover.py 1. %f SINR%05d' % (inner[i],i))
     data = np.loadtxt('SINR%05d' % (i))
-    SINR = np.append(SINR,np.dot(data,data)**2)
+    SINR = np.append(SINR,np.dot(data,data)**0.5/data.shape[0])
 
 plt.plot(inner,10*np.log10(SINR))
 plt.savefig('SINR.png')
