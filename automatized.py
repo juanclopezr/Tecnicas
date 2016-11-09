48fcc9e38e9a60cb1c516f72d6b12f497f8b2ee7
Juan Lopez <jc.lopez11@uniandes.edu.co>
2016-11-09 11:01:23 -0500

    fixed for smoother running

--- automatized.py
+++ automatized.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 import os
 
 inner = np.linspace(0.01,0.99,100)
-SINR = np.array([])
+SINR = np.zeros(100)
+avg = np.zeros(100)
 
-for i in range(inner.shape[0]):
-    os.system('python cover.py 1. %f SINR%05d' % (inner[i],i))
-    data = np.loadtxt('SINR%05d' % (i))
-    SINR = np.append(SINR,np.dot(data,data)**0.5/data.shape[0])
+for j in range(100):
+    for i in range(inner.shape[0]):
+        os.system('python cover.py 1. %f SINR%05d' % (inner[i],i))
+        data = np.loadtxt('SINR%05d' % (i))
+        SINR[i] = np.dot(data,data)**0.5/data.shape[0]
+    avg = avg + SINR/100.
 
 plt.plot(inner,10*np.log10(SINR))
 plt.savefig('SINR.png')
